f9e6647d7828284fa6e5fd588f78efbbd5f45749
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-07-11 00:24:57 +0300

    Adding initial stuff

---
+++ lib/file_mutex.rb
@@ -0,0 +1,39 @@
+#File based implementation of named mutexes in ruby.
+#Used for inter process synchronizaion withing the same local environment.
+#To start using it locks_dir should be set to some folder. 
+#Same folder should be set for each process in order to use same named mutex
+class FileMutex
+  def initialize(name)
+    @name = name
+    @lock_file_path = File.join(self.class.locks_dir, "#{name}.lock")
+  end
+  
+  #Acuires ownership on the mutext. Blocks untill the ownership obtained.
+  def acquire
+    @lock_file ||= File.open(@lock_file_path, File::RDWR|File::CREAT, 0644)
+    self
+  end
+  
+  #Releases ownership on the mutex so anyone else can acuire it.
+  def release
+    @lock_file.flock(File::LOCK_UN)
+    @lock_file.close
+    @lock_file = nil
+  end
+  
+  private
+  
+  class << self
+    def acuire(name)
+      new(name).acuire
+    end
+    
+    def locks_dir
+      @@locks_dir || raise "Locks dir has not been initialized."
+    end
+    
+    def locks_dir=(value)
+      @@locks_dir = value
+    end
+  end
+end
